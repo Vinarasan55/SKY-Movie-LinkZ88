ab6b490fdc911acdc19126da02079b422e494bdb
Vinarasan55 <122718267+Vinarasan55@users.noreply.github.com>
2023-04-19 09:56:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tinyfy.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'fbb20974f807df0fd4e2e69eedb930e96e3e242f')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', "True")),True)
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
